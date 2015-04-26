16513fce39c65c25466b2931b890be79729fbe68
Audrius <audrius@susumu.co.uk>
2015-04-26 23:20:16 +0100

    contact page update

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,14 +3,125 @@ var React = require('react');
 
 var Contact = React.createClass({
   getInitialState: function() {
-  	return {};
+    return {
+    	errors: {},
+    	submitted: false
+    }
+  },
+
+  // Handle submit event.
+  handleSubmit: function() {
+  	if (this.isValid()) {
+  		this.sendMessage();
+  	}
+  },
+
+  // Validate data.
+  isValid: function() {
+  	var errors = {},
+  			fields = ['name', 'email', 'message'];
+
+  	fields.forEach(function(field) {
+  		// Get field value.
+  		var value = trim(this.refs[field].getDOMNode().value);
+  		if (!value) {
+  			errors[field] = 'This field is required';
+  		}
+  	}.bind(this));
+  	// Set errors state.
+  	this.setState({
+  		errors: errors,
+  		submitted: false
+  	});
+
+  	var isValid = true;
+  	for (var error in errors) {
+  		isValid = false;
+  		break;
+  	}
+
+  	return isValid;
+  },
+
+  sendMessage: function() {
+  	// Lets not lose context.
+  	var ctx = this;
+  	$.ajax({
+  		type: "POST",
+  		url: "contact",
+  		data: this.getValues(),
+  		success: function(response) {
+  			this.setState({submitted: true});
+  		}
+  	})
+  	.fail(function() {
+	    ctx.setState({
+	    	errors: {server: 'Error sending message.'},
+	    	submitted: false
+	    });
+	  })
+  	
+  },
+
+  getValues: function() {
+  	return {
+  		name: this.refs.name.getDOMNode().value,
+  		email: this.refs.email.getDOMNode().value,
+  		message: this.refs.message.getDOMNode().value,
+  	}
+  },
+
+  // Render textfield field with error wrapper.
+  renderTextField: function(id, placeholder) {
+  	return <div className={(this.state.errors[id]) ? 'error input-wrapper' : 'input-wrapper' }>
+	  	  		<input type="text" className={"form-control " + id} id={id} ref={id} placeholder={placeholder} />
+	  	  		{ id in this.state.errors &&
+	  	  			<div className="error">{this.state.errors[id]}</div>
+	  	  		}
+  	  		 </div>
+  },
+
+  // Render textarea field with error wrapper.
+  renderTextArea: function(id, placeholder) {
+  	return <div className={(this.state.errors[id]) ? 'error input-wrapper' : 'input-wrapper' }>
+	  	  		<textarea className={"form-control " + id} id={id} ref={id} placeholder={placeholder} />
+	  	  		{ id in this.state.errors &&
+	  	  			<div className="error">{this.state.errors[id]}</div>
+	  	  		}
+  	  		 </div>
   },
 
   render: function() {
+  	var status = null;
+  	// Show notice message to user if message is sent successfully.
+  	if (this.state.submitted === true) {
+  		status = <div className="alert alert-success">Successfully sent.</div>
+  	}
+  	if (this.state.errors['server']) {
+  		status = <div className="alert alert-error">{this.state.errors['server']}</div>
+  	}
+
   	return (
-  	  <p>Get in touch form</p>
+  	  <div clasName="contact-form-wrapper">
+  	  	<div className="header-wrapper"><h2>Get in touch!</h2></div>
+  	  	<div className="alerts">{status}</div>
+  	  	<div className="form">
+  	  		{this.renderTextField("name", "Name")}
+  	  		{this.renderTextField("email", "Email")}
+  	  		{this.renderTextArea("message", "Message")}
+  	  		<button type="button" className="submit" onClick={this.handleSubmit}>Send!</button>
+  	  	</div>
+  	  </div>
   	);
-  }
+  },
+
 });
 
 module.exports = Contact;
+
+
+// Helper functions.
+function trim(string) {
+  var TRIM_RE = /^\s+|\s+$/g;
+  return string.replace(TRIM_RE, '');
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('node-jsx').install({extension: '.jsx'});
 var server = require('koa')();
-//var route = require('koa-route');
+var koa_router = require('koa-router');
 var serve = require('koa-static');
 var Router = require('react-router');
 var app = require('./app');
@@ -9,10 +9,39 @@ var navExpandAction = require('./actions/navExpand');
 var serialize = require('serialize-javascript');
 var React = require('react');
 var HtmlComponent = React.createFactory(require('./components/Html.jsx'));
+var parse = require('co-body');
+var nodemailer = require('nodemailer');
+var sendmailTransport = require('nodemailer-sendmail-transport');
+
+// Handle POST requests outside koa-router.
+server.use(koa_router(server))
+      .post('/contact', function *() {
+
+  var data = yield parse(this);
+
+  var mailOpts = {
+    from: data.email,
+    to: 'aubuka@gmail.com',
+    subject: 'AUDRIUS.IO Contact',
+    text: data.name + '<br/>' + data.message
+  };
+
+  var transporter = nodemailer.createTransport(sendmailTransport());
+  transporter.sendMail(mailOpts, function (error, response) {
+  });
+
+  this.body = 'sent';
+});
 
 server.use(serve('public', {defer: true}));
 
 server.use(function *() {
+
+  // Do nothing for POST requests.
+  if (this.request.method == "POST") {
+    return;
+  }
+
   var context = app.createContext();
   var ctx = this;
 
